a1ee9c83c19074a432bf5a3034a728da39af0fd0
kogo <kogo@DESKTOP-AH6JU31>
2021-08-25 12:21:12 -0700

    reflection

---
+++ Examples/Examples/CreateClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection;
+
+namespace Examples
+{
+    public static class CreateClass<T>
+        where T : class
+    {
+        public static object CreateClassObject(T param)
+        {
+            var type = param.GetType();
+
+            ConstructorInfo constructorInfo = type.GetConstructor(new Type[] { });
+            object obj = constructorInfo.Invoke(new object[] { });
+
+            return obj;
+        }
+    }
+}
---
+++ Examples/Examples/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Examples
+{
+    class Program
+    {
+        public class Person
+        {
+            private int x { get; set; }
+            private int y { get; set; }
+
+            private void Const()
+            {
+                Console.WriteLine("saba + sa");
+            }
+        }
+
+        class B : Person { }
+
+        static void Main(string[] args)
+        {
+            Type type = typeof(Person);
+
+            object person = CreateClass<Person>.CreateClassObject(new Person());
+
+            var members = type.GetProperties(BindingFlags.NonPublic | BindingFlags.Instance);
+
+            foreach (PropertyInfo item in members)
+            {
+                if (item.Name == "x")
+                {
+                    item.SetValue(person, 5);
+                    Console.WriteLine(item.GetValue(person));
+                }
+            }
+        }
+    }
+}
